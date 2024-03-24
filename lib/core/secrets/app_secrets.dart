f8eda2d84c88497fc467174af3bbff521fdef76b
deepanshu <deepanshukamlesh@gamil.com>
2024-03-24 16:02:18 +0530

    get_it dependency injection used in auth bloc user sign up

--- lib/core/secrets/app_secrets.dart
+++ lib/core/secrets/app_secrets.dart
@@ -1,4 +1,5 @@
 class AppSecrets {
-  static const supabaseUrl = '';
-  static const supabaseAnonKey = '';
+  static const supabaseUrl = 'https://cogyuliwjmanvqzgfvax.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNvZ3l1bGl3am1hbnZxemdmdmF4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTExNzgzMDEsImV4cCI6MjAyNjc1NDMwMX0.juUBVudGOy-k_tSq6uFfOOe9dNJVgeB_0IK3wlm-26o';
 }
