9c8f01e6b29585017e37a7e3ee02f42066058100
deepanshu <deepanshukamlesh@gamil.com>
2024-03-25 20:19:55 +0530

    feat: login user completed

--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -1,5 +1,6 @@
 import 'package:blog_app/core/secrets/app_secrets.dart';
 import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -37,9 +38,16 @@ void _initAuth() {
     ),
   );
 
+  serviceLocator.registerFactory(
+    () => UserLogin(
+      serviceLocator(),
+    ),
+  );
+
   serviceLocator.registerLazySingleton(
     () => AuthBloc(
       userSignUp: serviceLocator(),
+      userLogin: serviceLocator(),
     ),
   );
 }
