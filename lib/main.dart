10f914140a52365ab74a779d97ef73785c9607dc
deepanshu <deepanshukamlesh@gamil.com>
2024-03-26 22:28:45 +0530

    UI : add new blog page

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:blog_app/core/common/cubits/app_user/app_user_cubit.dart';
 import 'package:blog_app/core/theme/theme.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
 import 'package:blog_app/features/auth/presentation/pages/login_page.dart';
+import 'package:blog_app/features/blog/presentation/pages/blog_page.dart';
 import 'package:blog_app/init_dependencies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -52,11 +53,7 @@ class _MyAppState extends State<MyApp> {
         builder: (context, state) {
           //this state denotes if the user is logged in or not i.e. isLoggedIn
           if (state) {
-            return const Scaffold(
-              body: Center(
-                child: Text('LoggedIn !!!'),
-              ),
-            );
+            return const BlogPage();
           }
           return const LoginPage();
         },
