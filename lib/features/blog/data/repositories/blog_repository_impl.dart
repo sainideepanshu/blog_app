3f16f1a2d08be4875d37776123c815d1501c93ad
deepanshu <deepanshukamlesh@gamil.com>
2024-03-28 16:24:51 +0530

    feat : implemented Hive local storage

--- lib/features/blog/data/repositories/blog_repository_impl.dart
+++ lib/features/blog/data/repositories/blog_repository_impl.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
+import 'package:blog_app/core/constants/constants.dart';
 import 'package:blog_app/core/error/exceptions.dart';
 import 'package:blog_app/core/error/failures.dart';
+import 'package:blog_app/core/network/connection_checker.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_local_data_source.dart';
 import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:blog_app/features/blog/data/models/blog_model.dart';
 import 'package:blog_app/features/blog/domain/entities/blog.dart';
@@ -11,9 +14,13 @@ import 'package:uuid/uuid.dart';
 
 class BlogRepositoryImpl implements BlogRepository {
   final BlogRemoteDataSource blogRemoteDataSource;
+  final BlogLocalDataSource blogLocalDataSource;
+  final ConnectionChecker connectionChecker;
 
   BlogRepositoryImpl(
     this.blogRemoteDataSource,
+    this.blogLocalDataSource,
+    this.connectionChecker,
   );
 
   @override
@@ -25,6 +32,10 @@ class BlogRepositoryImpl implements BlogRepository {
     required List<String> topics,
   }) async {
     try {
+      if (!await (connectionChecker.isConnected)) {
+        return left(Failure(Constants.noConnectionErrorMessage));
+      }
+
       BlogModel blogModel = BlogModel(
         id: const Uuid()
             .v1(), // this id will be randomly generated , Supabase is not going to do it , we will use Uuid package
@@ -62,7 +73,14 @@ class BlogRepositoryImpl implements BlogRepository {
   @override
   Future<Either<Failure, List<Blog>>> getAllBlogs() async {
     try {
+      if (!await (connectionChecker.isConnected)) {
+        final blogs = blogLocalDataSource.loadBlogs();
+        return right(blogs);
+      }
+
       final blogs = await blogRemoteDataSource.getAllBlogs();
+      blogLocalDataSource.uploadLocalBlogs(blogs: blogs);
+
       return right(blogs);
     } on ServerException catch (e) {
       return left(Failure(e.message));
--- lib/init_dependencies.dart
+++ lib/init_dependencies.dart
@@ -5,6 +5,7 @@ import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
 import 'package:blog_app/features/auth/domain/usecases/current_user.dart';
 import 'package:blog_app/features/auth/domain/usecases/user_login.dart';
 import 'package:blog_app/features/auth/presentation/bloc/auth_bloc.dart';
+import 'package:blog_app/features/blog/data/datasources/blog_local_data_source.dart';
 import 'package:blog_app/features/blog/data/datasources/blog_remote_data_source.dart';
 import 'package:blog_app/features/blog/data/repositories/blog_repository_impl.dart';
 import 'package:blog_app/features/blog/domain/repositories/blog_repository.dart';
@@ -12,108 +13,13 @@ import 'package:blog_app/features/blog/domain/usecases/get_all_blogs.dart';
 import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
 import 'package:blog_app/features/blog/presentation/bloc/blog_bloc.dart';
 import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
 import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 import 'features/auth/data/datasources/auth_remote_data_source.dart';
 import 'features/auth/data/repositories/auth_repository_impl.dart';
 import 'features/auth/domain/usecases/user_sign_up.dart';
 
-final serviceLocator = GetIt.instance;
-
-Future<void> initDependencies() async {
-  _initAuth();
-  _initBlog();
-  final supabase = await Supabase.initialize(
-      url: AppSecrets.supabaseUrl, anonKey: AppSecrets.supabaseAnonKey);
-
-  serviceLocator.registerLazySingleton(() => supabase.client);
-
-  serviceLocator.registerFactory(() => InternetConnection());
-
-  serviceLocator.registerLazySingleton(
-    () => AppUserCubit(),
-  );
-
-  serviceLocator.registerFactory<ConnectionChecker>(
-    () => ConnectionCheckerImpl(
-      serviceLocator(),
-    ),
-  );
-}
-
-void _initAuth() {
-  serviceLocator
-    //Datasource
-    ..registerFactory<AuthRemoteDataSource>(
-      () => AuthRemoteDataSourceImpl(
-        serviceLocator(),
-      ),
-    )
-    //Repository
-    ..registerFactory<AuthRepository>(
-      () => AuthRepositoryImpl(
-        serviceLocator(),
-        serviceLocator(),
-      ),
-    )
-    //Use cases
-    ..registerFactory(
-      () => UserSignUp(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-      () => UserLogin(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-      () => CurrentUser(
-        serviceLocator(),
-      ),
-    )
-    //Bloc
-    ..registerLazySingleton(
-      () => AuthBloc(
-        userSignUp: serviceLocator(),
-        userLogin: serviceLocator(),
-        currentUser: serviceLocator(),
-        appUserCubit: serviceLocator(),
-      ),
-    );
-}
-
-void _initBlog() {
-  serviceLocator
-    // Datasource
-    ..registerFactory<BlogRemoteDataSource>(
-      () => BlogRemoteDataSourceImpl(
-        serviceLocator(),
-      ),
-    )
-    // Repository
-    ..registerFactory<BlogRepository>(
-      () => BlogRepositoryImpl(
-        serviceLocator(),
-      ),
-    )
-    // Use cases
-    ..registerFactory(
-      () => UploadBlog(
-        serviceLocator(),
-      ),
-    )
-    ..registerFactory(
-      () => GetAllBlogs(
-        serviceLocator(),
-      ),
-    )
-    // Bloc
-    ..registerLazySingleton(
-      () => BlogBloc(
-        uploadBlog: serviceLocator(),
-        getAllBlogs: serviceLocator(),
-      ),
-    );
-}
+part 'init_dependencies.main.dart';
